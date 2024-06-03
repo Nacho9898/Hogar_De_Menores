b9a82130530e85ecd9dc862cf242d2ea652640ea
Ignacio Ochoa <ig.ochoa@duocuc.cl>
2024-06-03 03:35:25 -0400

    Working on progress

--- Hogar_De_Menores.py
+++ Hogar_De_Menores.py
@@ -1,2 +1,43 @@
 
+# definiciones
 
+op_3 = 
+
+# Ingresar anotaciones/Observaciones de menores
+
+while True:
+    print()
+    print("usted ha elegido la opcion 3")
+    print()
+    print("1.- Ingresar observasiones del menor")
+    print("2.- Consultar observaciones del menor")
+    print("3.- Regresar al menu principal")
+    print()
+    input("por favor elige una opción: ")
+
+    if op_3 == "1":
+        print()
+        print("""usted ha elegido la opcion 1
+por favor ingrese los siguientes datos:
+""")
+        input("Nombres: ")
+        input("Apellidos: ")
+        input("Fecha de nacimiento: ")
+        input("RUT: ")
+        input("Sala: ")
+        input("Observaciones: ")
+
+    elif op_3 == "2":
+        print()
+        print("""usted ha elegido la opción 2
+Por favor ingrese los siguientes datos:
+""")
+        input("Ingrese el RUT del menor: ")
+        print()
+
+    elif op_3 == "3":
+        print()
+        print("""usted ha elegido la opción 3
+Sera enviado de regreso al menú principal
+""")
+        break
