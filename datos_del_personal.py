e9dccf5fde22cdb12ea7b875fae952cf48470e33
ignacio123451 <igr.munoz@duocuc.cl>
2024-06-12 21:11:22 -0400

    cambios ignacio muñoz

--- datos_del_personal.py
+++ datos_del_personal.py
@@ -5,30 +5,76 @@ while opc==2:
     print("Ingrese si quiere ver o agregar los datos del personal")
     print("1) Ver Datos del Personal")
     print("2) Agregar datos")
-    print("3) volver")
     res=int(input())
-    if res<1 or res>3:
+    if res<1 or res>2:
         print("Opción no valida")
     elif res==1:
         print(listpersonal)
         print("si desea volver precione cualquier tecla")
         res2=input()
     elif res==2:
-        print("ingrese los nombres")
-        nom=input()
-        listpersonal.append(nom)
-        print("ingrese los Apellidos")
-        ape=input()
-        listpersonal.append(ape)
-        print("ingrese Rut")
-        rut=input()
-        listpersonal.append(rut)
-        print("Ingrese su Nacionalidad")
-        nac=input()
-        listpersonal.append(nac)
-        print("Ingrese si es voluntario o personal activo")
-        vol_o_act=input()
-        listpersonal.append(vol_o_act)
-    elif res==3:
-        break
+
+        while (True):
+            print("ingrese el primer nombre")
+            nom1=input()
+            if nom1:
+                listpersonal.append(nom1)
+                break
+            else:
+                print("No se puede dejar en blanco")
+
+        while (True):
+            print("ingrese segundo nombre")
+            nom2=input()
+            if nom2:
+                listpersonal.append(nom2)
+                break
+            else:
+                print("No se puede dejar en blanco")
+
+        while (True):
+            print("ingrese primer apellido")
+            ape1=input()
+            if ape1:
+                listpersonal.append(ape1)
+                break
+            else:
+                print("No se puede dejar en blanco")
+
+        while (True):   
+            print("ingrese segundo apellido")
+            ape2=input()
+            if ape2:
+                listpersonal.append(ape2) 
+                break
+            else:
+                print("No se puede dejar en blanco")
+
+        while (True):
+            print("ingrese Rut")
+            rut=input()
+            if rut:
+                listpersonal.append(rut)
+                break
+            else:
+                print("No se puede dejar en blanco")
+        
+        while (True):
+            print("Ingrese su Nacionalidad")
+            nac=input()
+            if nac:
+                listpersonal.append(nac)
+                break
+            else:
+                print("No se puede dejar en blanco")
+        
+        while (True):
+            print("Ingrese si es voluntario o personal activo")
+            vol_o_act=input()
+            if vol_o_act:
+                listpersonal.append(vol_o_act)
+                break
+            else:
+                print ("No se puede dejar en blanco")
+        
             
