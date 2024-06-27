25154da619e76660f6eb83e9f580aa44dbdffec7
Ignacio Ochoa <ig.ochoa@duocuc.cl>
2024-06-27 04:15:40 -0400

    Updated

--- HDM_proyecto_final.py
+++ HDM_proyecto_final.py
@@ -1,115 +1,71 @@
-
-# Definiciones e imports
-import lb_op_1 as lb1
-import Login
-
-x = 0
-op = 0
-op1 = 0
-op2 = 0
-op3 = 0
-op4 = 0
-acceso_login = 0
-
-#############################################################
+import Libreria as lb
 
 # Login
-intentos = 0
-while intentos < 3:
-    usuario = input("Ingrese nombre de usuario: ")
-    contraseña = input("Ingrese Contraseña: ")
-    if Login.login(usuario, contraseña):
-        print("Bienvenido")
-        acceso_login = True
-        # Menú
-    while (x==0):
+acceso_permitido = lb.login()
+
+# Menú
+if acceso_permitido:
+    while True:
         print("""
 =====================================
 |                                   |
 |        Bienvenido al menú         |
 |         Hogar de menores          |
 |                                   |
-=====================================""")
-        print("1) Datos de menores")
-        print("2) Datos del personal")
-        print("3) Observaciones de menores")
-        print("4) Salir")
+=====================================
+1) Datos de menores
+2) Datos del personal
+3) Observaciones de menores
+4) Salir
+""")
         op = int(input("Por favor elige una opción: "))
-        print()
-        # Opciones
         if op == 1:
-            while op1 == 0:
-                print("Usted eligio la opción 1")
-                print("""
-    A continuación por favor
-    selecione una opción
+            print("""Usted ha elegido la opción 1
+Ahora que opción desea:
 
-    1) Ingresar datos del menor
-    2) Consultar datos del menor
-    """)
+1) Ingresar Datos del menor
+2) Consultar Datos del menor
+3) Volver al menú principal
+""")
+            while True:
                 op1 = int(input("Por favor elige una opción: "))
                 if op1 == 1:
-                    lb1.ingresar_datos()
-                    print("""
-    ¿Desea volver al menú de la opción 1?
-    1) Si
-    2) No
-    """)
-                    op2 = int(input("Por favor elige una opción: "))
-                    if op2 == 1:
-                        op1 = 0
+                    print("Usted ha elegido la opción 1")
+                    lb.ingresar_datos()
+                    break
                 elif op1 == 2:
-                    if datos_del_menor:
-                        # Solicitar el RUT para buscar los datos
-                        rut_consulta = input("Ingrese el RUT del menor que desea consultar: ")
-                        if "Rut" in datos_del_menor and datos_del_menor["Rut"] == rut_consulta:
-                            lb1.consultar_datos(datos_del_menor)  # Mostrar los datos si el RUT coincide
-                        else:
-                            print("El RUT ingresado no coincide con los datos ingresados anteriormente.")
-                    else:
-                        print("No se han ingresado datos de ningún menor.")
-                    print("""
-    ¿Desea volver al menu principal?
-    1) Si
-    2) Salir
-    """)
-            op2 = int(input("Por favor elige una opción: "))
-            if op2 == 1:
-                continue
-            elif op2 == 2:
-                break
-
-
+                    print("Usted ha elegido la opción 2")
+                    lb.consultar_datos()
+                    break
+                elif op1 == 3:
+                    print("Usted ha elegido la opción 3")
+                    break
         elif op == 2:
-            print("Usted eligio la opcion 2")
-            print("""
-    A continuación por favor
-    selecione una opción
-
-    1) Ingresar datos del personal
-    2) Consultar datos del personal
-    """)
-            
-
+            print("Usted ha elegido la opción 2")
+            lb.lista_del_personal1()
         elif op == 3:
-            print("Usted eligio la opcion 3")
-            print("""
-    A continuación por favor
-    selecione una opción
+            print("""Usted ha elegido la opción 3
+Ahora que opción desea:
 
-    1) Ingresar observaciones
-    2) Consultar consultar observaciones
-    """)
+1) Ingresar Observaciones
+2) Consultar Observaciones
+3) Volver al menú principal
+""")
+            while True:
+                op3 = int(input("Por favor elige una opción: "))
+                if op3 == 1:
+                    print("Usted ha elegido la opción 1")
+                    lb.ingresar_observaciones()
+                    break
+                elif op3 == 2:
+                    print("Usted ha elegido la opción 2")
+                    lb.consultar_observaciones()
+                    break
+                elif op3 == 3:
+                    print("Usted ha elegido la opción 3")
+                    break
         elif op == 4:
-            print("Usted eligio la opcion 4")
+            print("Gracias por usar el programa.")
             break
-    else:
-        intentos += 1
-        print("""Inicio de sesion incorrecto.
-
-Recuerda que Usuario y Contraseña deben ser iguales.
-te quedan""", 3 - intentos, "intentos")
-    break
-if intentos == 3:
-    print()
-    print("Se ha bloqueado su cuenta. Intentelo mas tarde")
+else:
+    print("Acceso denegado. Fin del programa.")
